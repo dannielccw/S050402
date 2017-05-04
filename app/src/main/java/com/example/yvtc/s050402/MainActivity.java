494f1ef9db9059274f722f04a998113815390abc
dannielccw <dannielccw@gmail.com>
2017-05-04 14:23:28 +0800

    BroadcastReceiver

---
+++ app/src/main/java/com/example/yvtc/s050402/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.yvtc.s050402;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click1(View v)
+    {
+        Intent it = new Intent();
+        it.setAction("My0504Action");
+        sendBroadcast(it);
+    }
+}
